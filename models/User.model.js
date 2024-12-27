9a9c5c87a2ba509cbf55a7dcd456c67cd1d20c7e
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-12-27 20:14:15 +0545

    user model add

---
+++ models/User.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = mongoose.model("User", UserSchema);
