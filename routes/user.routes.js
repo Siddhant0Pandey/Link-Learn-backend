aea0727c7d3148e71dfe0e1dcb1c6774233f50f7
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-12-28 13:33:59 +0545

    set function to hash the password

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,16 @@
 import express from "express";
+import crypto from "crypto";
 import { User } from "../models/User.model.js";
 
 const router = express.Router();
+
+function hashPassword(password, salt) {
+  return crypto.createHmac("sha256", salt).update(password).digest("hex");
+}
+
+function generateSalt() {
+  return crypto.randomBytes(16).toString("hex");
+}
 
 router.post("/register", async (req, res) => {
   try {
@@ -16,11 +25,19 @@ router.post("/register", async (req, res) => {
       return res.status(400).json({ message: "Username already exists" });
     }
 
-    const newUser = await User.create({ name, username, password });
+    const salt = generateSalt();
+    const hashedPassword = hashPassword(password, salt);
+
+    const newUser = await User.create({
+      name,
+      username,
+      password: hashedPassword,
+      salt,
+    });
 
     return res.status(201).json({
       message: "User registered successfully, Please go to Login Page",
-      user: newUser,
+      user: { name: newUser.name, username: newUser.username },
     });
   } catch (err) {
     console.error("Error during registration:", err);
@@ -33,18 +50,24 @@ router.post("/login", async (req, res) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-      return res.status(400).json({ message: "Both credential are required" });
+      return res.status(400).json({ message: "Both credentials are required" });
     }
 
     const existingUser = await User.findOne({ username });
     if (!existingUser) {
       return res.status(401).json({ message: "Incorrect username" });
     }
-    if (existingUser.password !== password) {
+
+    const hashedPassword = hashPassword(password, existingUser.salt);
+
+    if (hashedPassword !== existingUser.password) {
       return res.status(401).json({ message: "Incorrect password" });
     }
 
-    res.status(200).json({ message: "Login successful", user: existingUser });
+    res.status(200).json({
+      message: "Login successful",
+      user: { name: existingUser.name, username: existingUser.username },
+    });
   } catch (err) {
     console.error("Login error:", err);
     return res.status(500).json({ message: "Internal server error" });
