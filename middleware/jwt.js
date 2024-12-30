22b5afa31a475b7745442f177c75d81a67845576
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-12-30 14:35:52 +0545

    jwt middleware update

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,22 +1,24 @@
 import jwt from "jsonwebtoken";
-
 const jwtAuthMiddleware = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
+
   if (!token) {
-    return res.status(401).json({ error: "Unauthorised" });
+    return res.status(401).json({ error: "Unauthorized" });
   }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = decoded;
     next();
   } catch (err) {
     console.log(err);
-    res.status(401).json({ error: "invalid token" });
+    res.status(401).json({ error: "Invalid token" });
   }
 };
 
 const generateToken = (userData) => {
-  return jwt.sign(userData, process.env.JWT_SECRET_KEY);
+  return jwt.sign(userData, process.env.JWT_SECRET_KEY, { expiresIn: "1h" }); // Optional expiration time
 };
 
 export { jwtAuthMiddleware, generateToken };
