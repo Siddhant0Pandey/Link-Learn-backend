4d5edf9a7339b04f452b239b26b266b4afa70e81
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2025-01-05 17:26:24 +0545

    separate folder created for  activity model

---
+++ models/activities/EntertainmentLink.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const entertainmentlinkSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    url: { type: String, required: true },
+
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    timeSpent: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+export const EntertianmentLink = mongoose.model(
+  "EntertianmentLink",
+  entertainmentlinkSchema
+);
